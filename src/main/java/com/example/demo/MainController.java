946acef9a3ad7e058a5792302cfbbe4dd6b6a7d5
glend <glend@DESKTOP-GTH9QGR>
2020-05-01 17:04:44 -0600

    labo3actividad

--- src/main/java/com/example/demo/MainController.java
+++ src/main/java/com/example/demo/MainController.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import java.util.ArrayList;
 import java.util.Date;
 
 import org.springframework.stereotype.Controller;
@@ -10,9 +11,26 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class MainController {
 	
+	public ArrayList<alumno> listaAlumno = new ArrayList<alumno>(); 
+	
 	@GetMapping("/ingresar")
 	public String getForm(alumno alumno) {
 		return "ingresar";
+	}
+	
+	
+	@GetMapping("/lista")
+	public ModelAndView listaAl() {
+		ModelAndView mav = new ModelAndView();
+		
+		mav.setViewName("lista");
+		for (alumno alumno : listaAlumno) {
+			System.out.println(alumno.getNombres());
+		}
+		mav.addObject("studentlist", this.listaAlumno);
+		
+		
+		return mav;
 	}
 	
 	@PostMapping("/ingresar")
@@ -22,7 +40,10 @@ public class MainController {
 		ModelAndView mav = new ModelAndView();
 		
 		if(al.validate()) {
-			mav.setViewName("correcto");	
+			
+			mav.setViewName("correcto");
+			this.listaAlumno.add(al);
+			
 		}else {
 			mav.setViewName("ingresar");	
 			mav.addObject("msg", al.getMessageError());
